005ef58bd94da1141e95006d4ba741c1f63c43b8
Jay Zhang <>
2020-03-30 20:48:10 +0800

    feat: format time

--- python/date.py
+++ python/date.py
@@ -11,7 +11,7 @@ class MyArgs:
         # default positional arguments
         self.__parser.add_argument('-u', '--utc', help='Display UTC time', action='store_true')
         self.__parser.add_argument('-tz', '--tz', help='IANA Time Zone database Name, such as "America/New_York".')
-
+        self.__parser.add_argument('-f', '--format', help='Format string representation of the time, e.g., "%%Y-%%m-%%d %%H:%%M:%%S %%z". See more in "https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior". Some predefined formats: iso,ctime.')
         # parse args
         self.__args = self.__parser.parse_args()
 
@@ -21,7 +21,8 @@ class MyArgs:
 
 def get_current_time(utc, tzDataBaseName):
     
-    t = datetime.now().astimezone()
+    t = datetime.now()
+    t = t.astimezone()  # append local timezone
 
     if utc:
         return t.astimezone(pytz.utc)
@@ -31,12 +32,23 @@ def get_current_time(utc, tzDataBaseName):
 
     return t
 
+def time_to_str(t, format):
+
+    if not format:
+        return t.strftime('%a %b %d %T %Z %Y') # default, same with shell `date`'s default format
+
+    if format == 'iso':
+        return t.isoformat()
+    elif format == 'ctime':
+        return t.ctime()
+    return t.strftime(format)
+
 
 def main():
     args = MyArgs().get_args()
 
     t = get_current_time(args.utc, args.tz)
-    print(t)
+    print(time_to_str(t, args.format))
 
 if __name__ == '__main__':
     main()
